7d2f583f6431e5909e0d2121029e0b6fabfc68b3
Sakib Radh <sadman.sakib@dreamonline.co.jp>
2023-05-24 00:13:39 +0600

    Database table and post methods are setup

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DataInsertion;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/transaction', [DataInsertion::class, 'insertTransaction']);
+
+Route::post('/account-head', [DataInsertion::class, 'insertAccountHead']);
+
+Route::post('/group', [DataInsertion::class, 'insertGroup']);
